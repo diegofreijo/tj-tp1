afb49f9022ff51622be6234ee872db450aaf005a
giga.freijo <giga.freijo@9d8b7889-944d-0410-8b1f-b3d1d070b0e3>
2008-05-19 12:50:35 +0000

    Hice el juego con las estrategias de Pecifico y PasoAPaso.

---
+++ src/Demonios/Demonios/PasoAPaso.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JuegoDemonios;
+
+namespace JuegoDemonios.Demonios
+{
+	using Casilla = Int32;
+
+	class PasoAPaso : Demonio
+	{
+		public PasoAPaso(int n, int k) : base(n, k)
+		{	
+		}
+
+		/// <summary>
+		/// Elige el casillero mas cercano
+		/// </summary>
+		protected override Casilla ElegirJugada(Tablero tablero, Casilla posicion, Casilla oponente, int k)
+		{
+			List<Casilla> posibles = JugadasPosibles(tablero, posicion, k);
+			Casilla min = posibles[0];
+			for (int i = 1; i < posibles.Count; ++i)
+			{
+				if (Math.Abs(posibles[i] - posicion) < Math.Abs(min - posicion))
+					min = posibles[i];
+			}
+
+			return min;
+		}
+	}
+}
---
+++ src/Demonios/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuegoDemonios
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = 50;
+			int k = 7;
+			Demonio jugador1 = new Demonios.PasoAPaso(n, k);
+			Demonio jugador2 = new Demonios.Pacifico(n, k);
+			
+			Juego juego = new Juego(n, k, jugador1, jugador2);
+			ResultadoDelJuego resultado = juego.ComenzarJuego();
+
+			switch(resultado)
+			{
+				case ResultadoDelJuego.Empataron:
+					Console.WriteLine("Empataron");
+					break;
+				case ResultadoDelJuego.Gano1:
+					Console.WriteLine("Gano 1");
+					break;
+				case ResultadoDelJuego.Gano2:
+					Console.WriteLine("Gano 2");
+					break;
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
